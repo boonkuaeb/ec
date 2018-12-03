6d9a8d3963c2ee25142211d15f1bb51388f20964
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-12-03 14:52:01 +0700

    custom API exporsed

--- src/main/java/explorecali/repository/TourRepository.java
+++ src/main/java/explorecali/repository/TourRepository.java
@@ -1,11 +1,36 @@
 package explorecali.repository;
 
 import explorecali.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
+public interface TourRepository extends PagingAndSortingRepository<Tour, Long> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
-public interface TourRepository extends CrudRepository<Tour, Long> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
